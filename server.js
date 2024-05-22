77ff79f85feedcbafe8aa5e7907f5d0f633607c8
Raunak-Srivastava <2021ugee081@nitjsr.ac.in>
2024-05-22 19:48:23 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ app.use('/api/users/', usersRoute)
 app.use('/api/bookings/', bookingsRoute)
 
 
-const path = require('path')
+// const path = require('path')
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/' , express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/' , express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 
 
