cfe3a3f55fdfa3f420272cf1418694ef40cba294
Raunak-Srivastava <2021ugee081@nitjsr.ac.in>
2024-05-22 20:32:23 +0530

    Updated db.js

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB(){
 
-    // mongoose.connect('mongodb+srv://admin:pzJHERiOr02lfDBc@cluster0.98wma.mongodb.net/rohitcars' , {useUnifiedTopology: true , useNewUrlParser: true})
-
-    mongoose.connect("mongodb://raunaksrivastava6762:Raunak%40123@ac-pie29bm-shard-00-00.krvenlk.mongodb.net:27017,ac-pie29bm-shard-00-01.krvenlk.mongodb.net:27017,ac-pie29bm-shard-00-02.krvenlk.mongodb.net:27017/urbanrentals?ssl=true&replicaSet=atlas-ntjq9c-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb://user2000:1234@ac-pie29bm-shard-00-00.krvenlk.mongodb.net:27017,ac-pie29bm-shard-00-01.krvenlk.mongodb.net:27017,ac-pie29bm-shard-00-02.krvenlk.mongodb.net:27017/urbanrentals?ssl=true&replicaSet=atlas-ntjq9c-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
 
     const connection = mongoose.connection
 
